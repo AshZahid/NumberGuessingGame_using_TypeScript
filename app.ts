5c399fb12dd73bd4d1e9427d6eb6384baa0184d2
AsmaZahid <168186482+AshZahid@users.noreply.github.com>
2024-10-09 17:34:33 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,31 @@
+//Number Guessing Game
+import inquirer from "inquirer";
+
+const randomnumber = Math.floor(Math.random()*9+1);
+
+const answer = await inquirer.prompt([
+    {
+        name: "guessnumber",
+        type: "number",
+        message: "Enter your guess number between 1 to 8:",
+
+    }
+]
+)
+console.log(answer)
+
+if(answer.guessnumber === randomnumber){
+    console.log("Congratulation you guess right number")
+}
+else{
+    console.log("Sorry your guess number is wrong try again")
+}
+
+
+
+
+
+
+
+
+
